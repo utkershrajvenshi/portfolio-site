65508742c5eadd8c0e2df9d64013290ed5f5f865
utkershrajvenshi <utkarshrajvanshi2@gmail.com>
2024-06-21 10:57:32 +0000

    Integrated Next, Tailwind and developed landing page

---
+++ app/page.tsx
@@ -0,0 +1,22 @@
+import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import { FaHome } from "react-icons/fa";
+
+export default function Home() {
+  return (
+    <main className="relative bg-black-100 flex flex-col items-center justify-center overflow-hidden mx-auto px-5 sm:px-10">
+      <div className="w-full max-w-7xl">
+        <FloatingNav
+          navItems={[
+            {
+              name: 'Home',
+              link: '/',
+              icon: <FaHome />
+            }
+          ]}
+        />
+        <Hero />
+      </div>
+    </main>
+  );
+}
