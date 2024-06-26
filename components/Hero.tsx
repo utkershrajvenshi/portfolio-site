82235f5b506e7f018a044b00a99fa00ec119f1a9
Utkersh Rajvenshi <utkarshrajvanshi2@gmail.com>
2024-06-27 01:39:56 +0530

    Made changes as per review

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -18,12 +18,12 @@ const Hero = () => {
       </div>
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
-          <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>Hey there!</h2>
+          <h2 className='uppercase tracking-widest text-xl text-center text-blue-100 max-w-80'>Hey there, I&apos;m Utkersh!</h2>
           <TextGenerateEffect
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
             words="Building beautiful, responsive front-ends" />
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi, I&apos;m Utkersh, a passionate front-end developer based out of India crafting user-centric interfaces with React, Next.js, TypeScript & Tailwind CSS
+            A passionate front-end developer based out of India crafting user-centric interfaces with React, Next.js, TypeScript & Tailwind CSS
           </p>
           <a href="#about">
             <MagicButton
